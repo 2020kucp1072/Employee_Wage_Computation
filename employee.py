84449a8d65df1797506e5c4325db9d00346f46ea
Vemula Dileep <vemuladileep082002@gmail.com>
2024-10-03 17:51:04 +0530

    [VEMULA add: python program to check whether employee is present or absent]

--- employee.py
+++ employee.py
@@ -15,8 +15,25 @@ def welcome_message():
     """
     print("Welcome to Employee Wage Computation Program")
 
+import random
+
+def is_employee_present():
+    """
+    Description:
+        Checks whether the employee is present or absent using random.
+    Return:
+        bool : True if employee is present, False if absent
+    """
+    return random.randint(0,1)
+
 def main():
     welcome_message()
+    present = is_employee_present()
+    if present==1:
+        print("Employee is Present")
+    else:
+        print("Employee is Absent")
 
 if __name__ == "__main__":
     main()
+
