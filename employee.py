f7e7f44f878af68ca71f3633dd272b04331d2bff
Vemula Dileep <vemuladileep082002@gmail.com>
2024-10-03 16:51:58 +0530

    [VEMULA add: python program to display welcome message]

---
+++ employee.py
@@ -0,0 +1,22 @@
+"""
+    @Author: VEMULA DILEEP
+    @Date: 02-10-2024
+    @Last Modified by: VEMULA DILEEP
+    @Last Modified time: 17:30
+    @Title: Employee Wage Computation Program
+"""
+
+def welcome_message():
+    """
+    Description:
+        Displays a welcome message for the Employee Wage Computation Program.
+    Return:
+        None
+    """
+    print("Welcome to Employee Wage Computation Program")
+
+def main():
+    welcome_message()
+
+if __name__ == "__main__":
+    main()
